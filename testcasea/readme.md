fd8c8fe141c594c09e412b6aa6cceaaf6cf29421
liuting <892895953@qq.com>
2024-04-10 09:31:02 +0800

    Clear git cache

--- testcasea/readme.md
+++
@@ -1,1 +0,0 @@
-"#testhuahua" 
--- testcasea/test_idle.py
+++
@@ -1,9 +0,0 @@
-from page.app import App
-
-
-class TestIdle:
-    def setup(self):
-        self.idle = App().start().main().goto_idle()
-
-    def test_idle(self):
-        assert "二手" in self.idle.idle()
